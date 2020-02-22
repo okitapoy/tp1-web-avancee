d7cab2e0f144c0fbab7a47fa994d56459737ff29
okitapoy <okitapoy@gmail.com>
2020-02-21 19:51:14 -0500

    dd

--- database.py
+++ database.py
@@ -1,1 +1,31 @@
+import sqlite3
 
+
+class Database:
+
+    def __init__(self):
+        self.connection = None
+
+
+    def get_connection(self):
+        self.connection = sqlite3.connect('db/articles.db')
+        return self.connection
+
+
+    def disconnect(self):
+        if self.connection is not None:
+            self.connection.close()
+
+
+    def add_article(self,id,titre,identifiant,auteur,date_publication,paragraphe):
+        connection = self.get_connection()
+        connection.execute(("insert into article(id,titre,identifiant,auteur,date_publication,paragraphe)"
+                            "values(?,?,?,?,?,?)"),(id,titre,identifiant,auteur,date_publication,paragraphe))
+        connection.commit()
+
+
+    def get_article_complet(self):
+        cursor = self.get_connection().cursor()
+        cursor.execute("select id,titre,identifiant,auteur,date_publication,paragraphe from article")
+        article = cursor.fetchall()
+        return [{"id": article[0] ,"titre": article[1],"identifiant": article[2],"auteur": article[3],"date_publication": article[4],"paragraphe": article[5]} for each_line in article]
--- index.py
+++ index.py
@@ -8,15 +8,33 @@ from flask import g
 from flask import request
 from flask import redirect
 from flask import Response
-#from .database import Database
+from .database import Database
 #import uuid
 
 app = Flask(__name__)
 
 
+def get_db():
+    db = getattr(g,'_database',None)
+    if db is None:
+        g._database = Database()
+    return g._database
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g,'_database',None)
+    if db is not None:
+        db.disconnect()
+
 
 @app.route('/')
 def page_acceuil():
+    db = get_db()
+    rows = db.get_article_complet()
+
+    #for row in rows:
+    print (row)
     return ("page acceuil")
 
 
